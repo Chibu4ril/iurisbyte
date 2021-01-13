c2ecbd3659f2c4d83f48d219e492cd37f9833c46
chibu4ril <successskido@gmail.com>
2021-01-13 21:55:57 +0800

    Navbar

---
+++ legal/legals/src/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Image,
+  Container,
+  NavDropdown,
+} from "react-bootstrap";
+import logo1 from "./components/images/logo.png";
+
+function Navbarr() {
+  return (
+    <>
+      <Container>
+        <Navbar collapseOnSelect expand="lg">
+          <Navbar.Brand href="#home">
+            <Image src={logo1} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto  ml-auto">
+              <NavDropdown title="LEGAL SERVICES" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#business">
+                  Business Law
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#criminal">
+                  Criminal Law
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#family">Family Law</NavDropdown.Item>
+                <NavDropdown.Item href="#personal">
+                  Personal Law
+                </NavDropdown.Item>
+              </NavDropdown>
+              {/* <Nav.Link href="#home">Home</Nav.Link> */}
+              <Nav.Link href="#features">HOW IT WORKS</Nav.Link>
+              <NavDropdown title=" COMPANY" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#business">About</NavDropdown.Item>
+                <NavDropdown.Item href="#criminal">Blog</NavDropdown.Item>
+                <NavDropdown.Item href="#family">Family Law</NavDropdown.Item>
+                <NavDropdown.Item href="#personal">Contact</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+
+            <Button variant="outline-info">Post My Services</Button>
+            {/* <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        </Form> */}
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </>
+  );
+}
+
+export default Navbarr;
